ee696493817a21dafced70d17bc86087dea3f9bc
Jon Seaton <jonathanseaton29@gmail.com>
2018-12-14 00:00:42 -0700

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,37 +4,70 @@ var questions = [
     {
         question: "1. Who is the greatest Jazz player of all time?",
         answerChoices: ["Raja Bell", "Gordan Gerrick", "Karl Malone", "Joe Ingles"],
-        correctAnswer: ["Joe Ingles"]
+        correctAnswer: "Joe Ingles"
     },
     {
         question: "2. What year did the Jazz go to the NBA Finals?",
         answerChoices: ["97,98", "96,97", "98,08", "They haven't at all."],
-        correctAnswer: ["97,98"]
+        correctAnswer: "97,98"
     },
     {
         question: "3. What Jazz player was in the 2017 NBA All-Star game?",
         answerChoices: ["Rudy Gobert", "Derrick Favors", "Joel Bolomboy", "Gordon Hayward"],
-        correctAnswer: ["Gordan Hayward"]
+        correctAnswer: "Gordon Hayward"
     },
     {
         question: "4. What year did the Jazz come to Utah?",
         answerChoices: ["1972", "1991", "1979", "1989"],
-        correctAnswer: ["1979"]
+        correctAnswer: "1979"
     }
 ]
 $(document).ready(function () {
     $('#start').on('click', function () {
         $('.jazzNote').hide();
         $('.display-3').hide();
-        $(".display-3").replaceWith("<h1>Timer: 30</h1>");
+        var timer = $("<h1>")
+        timer.text("Time Remaining: ")
+        var timeLeft = $("<span>")
+        $(timeLeft).attr("id", "timer");
+        timer.append(timeLeft);
+        $(".display-3").replaceWith(timer);
         var count = 30;
-        var intId = function counter() { 
-            document.getElementById("#time-left")
-            count++;
-        }
-        // Timer for 30 seconds to answer the questions
+        // var intId = function counter() { 
+        //     document.getElementById("#time-left")
+        //     count++;
+        // }
+        $("#timer").text(count)
+        setInterval(function(){
+             count --; 
+             $("#timer").text(count)
+             if (count === 0) {
+                 scoreGame();
+                 clearInterval(fucntion())
+             }
+             
+            }, 1000);
+
+            
             startGame();
     });
+    function scoreGame() {
+        var radioBtn = $('input:checked');
+        console.log(radioBtn);
+        var score = 0;
+        for (var j = 0; j < radioBtn.length; j++) {
+            if ($(radioBtn[j]).attr("choice") === questions[j].correctAnswer) {
+                score+=1;
+            }
+        }
+        alert("you win you got" + score);
+    }
+    function makeButton() {
+        var newBtn = $("<button>")
+        newBtn.text("Score the game");
+        newBtn.on("click", scoreGame);
+        return newBtn;
+    }
     function startGame() {
         var questionZone = $("#question-zone")
         for (var i = 0; i < questions.length; i++) {
@@ -49,16 +82,18 @@ $(document).ready(function () {
                 var currentAnswer = currentQuestion.answerChoices[j];
                 var answerChoice = $('<input type="radio" name = "question"> ' + currentAnswer + '</input>');
                 answerChoice.attr({
-                    name: ("question" + i)
+                    name: ("question" + i),
+                    choice: currentAnswer
                 })
                 answerBox.append(answerChoice);
             }
-
             questionStem.text(questions[i].question)
             questionBox.append(questionStem)
             questionBox.append(answerBox);
             questionZone.append(questionBox);
         }
+        var submitBtn = makeButton();
+        questionZone.append(submitBtn);
     }
 
 });
